88b6a438b16f0f9aa207fc0a72677809e20bbfae
sardorbek-matniyazov <sardorbekmatniyazov03@gmail.com>
2023-02-17 13:33:25 +0500

    address from for users order bug solved

--- src/main/java/saiga/utils/validations/UserOrderDirectionHandler.java
+++ src/main/java/saiga/utils/validations/UserOrderDirectionHandler.java
@@ -15,6 +15,6 @@ public class UserOrderDirectionHandler implements ConstraintValidator<CheckUserD
     public boolean isValid(DirectionRequest directionRequest, ConstraintValidatorContext constraintValidatorContext) {
         return directionRequest != null
                 && directionRequest.addressFrom() != null
-                && (directionRequest.addressFrom().lat() != 0.0 && directionRequest.addressFrom().lon() != 0.0 && directionRequest.addressFrom().title() != null);
+                && ((directionRequest.addressFrom().lat() != 0.0 && directionRequest.addressFrom().lon() != 0.0) || directionRequest.addressFrom().title() != null);
     }
 }
